a9f4614b833bddb85c78153248faf3b6a746bbf9
David Murillo <dmz866@hotmail.com>
2025-02-25 21:55:10 -0500

    mongo db added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data/mongo/mongo-database';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
 
-(async()=> {
-  main();
-})();
-
-
-function main() {
-
-  const server = new Server({
-    port: envs.PORT,
-    routes: AppRoutes.routes,
-  });
-
-  server.start();
+async function main() {
+    await MongoDatabase.connect({ url: envs.MONGO_URL, dbName: envs.MONGO_DB_NAME });
+    
+    const server = new Server({ port: envs.PORT, routes: AppRoutes.routes });
+    server.start();
 }
+
+main();
